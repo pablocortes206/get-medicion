e944f0e8a4eb5fa9d41f1660720f4db31b9f8dc1
pablocortes206 <pablo.cortes.ramos@ua.cl>
2026-04-16 12:20:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ from typing import List, Tuple, Optional, Dict
 
 import pandas as pd
 import streamlit as st
+from supabase import create_client
+
+@st.cache_resource
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = get_supabase()
 
 # ==============================
 # ESTILO TECK
